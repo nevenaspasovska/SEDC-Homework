46e1e9e04ed3ba7ad157bd6a23e5189f367a87fc
nevenaspasovska <47951834+nevenaspasovska@users.noreply.github.com>
2019-03-03 16:40:25 +0100

    Add files via upload

---
+++ Nevena Spasovska_Homework_class01/Nevena Spasovska_Homework_class01/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nevena_Spasovska_Homework_class01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int spaces = 8;
+            int asterix = 1;
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < spaces; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 0; j < asterix; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+                asterix++;
+                spaces--;
+            }
+            
+        }
+    }
+}
